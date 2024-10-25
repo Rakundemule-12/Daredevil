5cc637628156d2a822a008ba2155270a2b6cc221
Pavan <pavanrku@gmail.com>
2024-10-25 11:35:38 +0530

    Java Programs

--- src/test/java/org/example/JavaPrograms/Locators_VWO.java
+++ src/test/java/org/example/JavaPrograms/Locators_VWO.java
@@ -20,11 +20,11 @@ public class Locators_VWO {
 
 
         WebDriver driver = new ChromeDriver(edgeOptions);
-        driver.navigate().to("https://app.vwo.com");
-        System.out.println(driver.getTitle());
+        driver.navigate().to("https://app.vwo.com/#/login");
+//        System.out.println(driver.getTitle());
 
-        Assert.assertEquals(driver.getTitle(),"Login - VWO");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://app.vwo.com/#/login");
+//        Assert.assertEquals(driver.getTitle(),"Login - VWO");
+//        Assert.assertEquals(driver.getCurrentUrl(), "https://app.vwo.com/#/login");
 
         //Find the email input by ID and enter the email
         //<input type="email" class="text-input W(100%)" name="username" id="login-username" data-qa="hocewoqisi">
@@ -32,12 +32,23 @@ public class Locators_VWO {
         //<button type="submit" id="js-login-btn" class="btn btn--positive btn--inverted W(100%) H(48px) Fz(16px)" onclick="login.login(event)" data-qa="sibequkica">
         //<div class="notification-box-description" id="js-notification-box-msg" data-qa="rixawilomi">Your email, password, IP address or location did not match</div>
 
+//      below is the code for negative testing
+//          WebElement emailinputbox  =driver.findElement(By.id("login-username"));
+//          emailinputbox.sendKeys("admin@admin.com");
+//
+//        WebElement passwordbox  =driver.findElement(By.id("login-password"));
+//        passwordbox.sendKeys("mamemem");
+//
+//        WebElement buttonclick  =driver.findElement(By.id("js-login-btn"));
+//        buttonclick.click();
 
-          WebElement emailinputbox  =driver.findElement(By.id("login-username"));
-          emailinputbox.sendKeys("admin@admin.com");
+
+        //      below is the code for positive testing
+        WebElement emailinputbox  =driver.findElement(By.id("login-username"));
+        emailinputbox.sendKeys("poldberg1947@gmail.com");
 
         WebElement passwordbox  =driver.findElement(By.id("login-password"));
-        passwordbox.sendKeys("mamemem");
+        passwordbox.sendKeys("Team@123");
 
         WebElement buttonclick  =driver.findElement(By.id("js-login-btn"));
         buttonclick.click();
@@ -45,15 +56,23 @@ public class Locators_VWO {
         //after 3 seconds an error message should come, please add try catch block or wait method else test case fails
         //because we have not waited
 
+        //<h1 class="page-heading" data-qa="page-heading">Dashboard</h1>
+        //h1[@class = "page-heading"]
+
         try {
-            Thread.sleep(3000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        WebElement notified = driver.findElement(By.id("js-notification-box-msg"));
-        Assert.assertEquals(notified.getText(),"Your email, password, IP address or location did not match");
 
-       driver.quit();
+        WebElement dashboard_page = driver.findElement(By.xpath("//h1[@class = 'page-heading']"));
+        Assert.assertEquals(dashboard_page.getText(),"Dashboard");
+
+
+
+        //WebElement notified = driver.findElement(By.id("js-notification-box-msg"));
+        //Assert.assertEquals(notified.getText(),"Your email, password, IP address or location did not match");
+
     }
 
 
