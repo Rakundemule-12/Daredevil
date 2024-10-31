e7431b9e709ead55accf3e43f781f3c82837af8c
Pavan <pavanrku@gmail.com>
2024-10-31 17:48:43 +0530

    Selenium Projects

--- src/test/java/org/example/Selenium_Projects/SVG_Maps.java
+++ src/test/java/org/example/Selenium_Projects/SVG_Maps.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class SVG_Maps {
@@ -28,7 +31,8 @@ public class SVG_Maps {
 
     @Description("Verify that the tripura is in india and click on it.")
     @Test
-    public void test_svg_india_search_click() {
+    public void test_svg_india_search_click()
+    {
 
         driver.manage().window().maximize();
         String URL = "https://www.amcharts.com/svg-maps/?map=india";
@@ -36,13 +40,16 @@ public class SVG_Maps {
         driver.manage().window().maximize();
         // local-name() , name()- Xpath (built in)
         // document.querySelectorAll("#login-username"); - JS
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
         List<WebElement> states = driver.findElements
                 (By.xpath("//*[local-name()='svg']/*[local-name()='g'][7]/*[local-name()='g']/*[local-name()='g']/*[local-name()='path']"));
         for (WebElement state : states) {
             System.out.println(state.getAttribute("aria-label"));
-            if (state.getAttribute("aria-label").contains("Gujarat")) {
-                state.click();
+            if (state.getAttribute("aria-label").contains("Tripura")) {
+                wait.until(ExpectedConditions.elementToBeClickable(state)).click();
             }
+
         }
 
 
